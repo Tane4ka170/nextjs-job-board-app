7fd5368529023517184819c72bdbdca9d6cbede9
Tane4ka170 <tane4ka170@gmail.com>
2024-10-03 23:11:14 +0300

    Added JobRow component for displaying job listings

---
+++ src/app/components/JobRow.tsx
@@ -0,0 +1,21 @@
+export default function JobRow() {
+  return (
+    <>
+      <div className="bg-white p-4 rounded-lg shadow-sm flex gap-4">
+        <div>
+          <img
+            src="https://freepnglogo.com/images/all_img/1717224836starbucks-logo-transparent.png"
+            alt="logo"
+            className="size-12"
+          />
+        </div>
+        <div>
+          <div>Starbucks</div>
+          <div>Barista</div>
+          <div>On-site | Evesham, UK | Full-time, part-time</div>
+        </div>
+        <div>created</div>
+      </div>
+    </>
+  );
+}
