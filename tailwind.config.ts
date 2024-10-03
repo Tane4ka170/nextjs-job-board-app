f9ee43a5ab8818cdb26ffb0cb4e3c97b5b913030
Tane4ka170 <tane4ka170@gmail.com>
2024-10-03 23:16:49 +0300

    Added container with responsive breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,14 @@ const config: Config = {
   theme: {
     container: {
       screens: {
-        'lg':	'max-width: 1024px';
-'xl':'max-width: 1280px';
-'2xl':	'max-width: 1536px';
-      }
+        lg: "max-width: 1024px",
+        xl: "max-width: 1280px",
+        "2xl": "max-width: 1536px",
+      },
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+      },
     },
     extend: {
       colors: {
