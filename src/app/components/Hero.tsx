5c17ac0037e523698ea604b2b7635d4b7abb76e4
Tane4ka170 <tane4ka170@gmail.com>
2024-10-04 23:16:36 +0300

    Updated Hero section styles & commented paragraph

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,12 +1,12 @@
 export default function Hero() {
   return (
-    <section className="container py-12">
+    <section className="container my-16">
       <h1 className="text-4xl font-bold text-center">
         Discover your ideal <br /> career
       </h1>
-      <p className="text-center text-neutral-600 mt-2">
+      {/* <p className="text-center text-neutral-600 mt-2">
         Find your dream job. Your ideal career is waiting for you!
-      </p>
+      </p> */}
       <form className="flex gap-2 mt-4 max-w-xl mx-auto">
         <input
           type="search"
