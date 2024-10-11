db77648262c2f547b7b094d1a5c6b089ec9c049c
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-10-11 19:18:24 +0300

    Require login & show active organizations for new listing

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -4,22 +4,72 @@ import { withAuth } from "@workos-inc/authkit-nextjs";
 
 export default async function NewListingPage() {
   const { user } = await withAuth();
+  const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+  if (!user) {
+    return (
+      <div className="container">
+        <div>You need to be logged in to post a job</div>
+      </div>
+    );
+  }
+
+  const organizationMemberships =
+    await workos.userManagement.listOrganizationMemberships({
+      userId: user.id,
+    });
+
+  const activeOrganizationMemberships = organizationMemberships.data.filter(
+    (om) => om.status === "active"
+  );
+  const organizationsNames: { [key: string]: string } = {};
+  for (const activeMembership of activeOrganizationMemberships) {
+    const organization = await workos.organizations.getOrganization(
+      activeMembership.organizationId
+    );
+    organizationsNames[organization.id] = organization.name;
+  }
+
   return (
     <div className="container">
-      {!user && <div> You need to be logged in for posting a job</div>}
-      {user && (
+      <div>
+        <h2 className="text-lg mt-6">Your companies</h2>
+        <p className="text-gray-500 text-sm mb-2">
+          Select a company to create a job add for
+        </p>
         <div>
-          <form action="">
-            <h2 className="text-lg">Create a company</h2>
-            <p className="text-neutral-500 text-sm">
-              To create a job listing your first need to register a company
-            </p>
-            <button className="flex gap-2 items-center bg-neutral-200 px-4 py-2 rounded-md">
-              Create a company <FontAwesomeIcon className="h-4" icon={faArrowAltCircleRight}
-            </button>
-          </form>
+          <div className="border inline-block rounded-md">
+            {Object.keys(organizationsNames).map((orgId) => (
+              <Link
+                href={"/new-listing/" + orgId}
+                className={
+                  "py-2 px-4 flex gap-2 items-center " +
+                  (Object.keys(organizationsNames)[0] === orgId
+                    ? ""
+                    : "border-t")
+                }
+              >
+                {organizationsNames[orgId]}
+                <FontAwesomeIcon className="h-4" icon={faArrowRight} />
+              </Link>
+            ))}
+          </div>
         </div>
-      )}
+
+        {organizationMemberships.data.length === 0 && (
+          <div className="border border-blue-200 bg-blue-50 p-4 rounded-md">
+            No companies found assigned to your user
+          </div>
+        )}
+
+        <Link
+          className="inline-flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md mt-6"
+          href={"/new-company"}
+        >
+          Create a new company
+          <FontAwesomeIcon className="h-4" icon={faArrowRight} />
+        </Link>
+      </div>
     </div>
   );
 }
