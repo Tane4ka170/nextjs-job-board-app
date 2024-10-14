3bfd3451072dce24253b50d1e1d5703910dac660
Tane4ka170 <tane4ka170@gmail.com>
2024-10-14 16:44:49 +0300

    Refactored NewListingPage for auth

--- src/app/new-listing/page.tsx
+++ src/app/new-listing/page.tsx
@@ -1,10 +1,18 @@
-import { faArrowAltCircleRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { withAuth } from "@workos-inc/authkit-nextjs";
+'use server';
+import {createCompany} from "@/app/actions/workosActions";
+import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {getUser} from "@workos-inc/authkit-nextjs";
+import {WorkOS} from "@workos-inc/node";
+import Link from "next/link";
 
 export default async function NewListingPage() {
-  const { user } = await withAuth();
+
   const workos = new WorkOS(process.env.WORKOS_API_KEY);
+
+  const {user} = await getUser();
+
+
 
   if (!user) {
     return (
@@ -14,41 +22,32 @@ export default async function NewListingPage() {
     );
   }
 
-  const organizationMemberships =
-    await workos.userManagement.listOrganizationMemberships({
-      userId: user.id,
-    });
+  const organizationMemberships = await workos.userManagement.listOrganizationMemberships({
+    userId: user.id,
+  });
 
-  const activeOrganizationMemberships = organizationMemberships.data.filter(
-    (om) => om.status === "active"
-  );
-  const organizationsNames: { [key: string]: string } = {};
+  const activeOrganizationMemberships = organizationMemberships.data.filter(om => om.status === 'active');
+  const organizationsNames:{[key: string]: string} = {};
   for (const activeMembership of activeOrganizationMemberships) {
-    const organization = await workos.organizations.getOrganization(
-      activeMembership.organizationId
-    );
+    const organization = await workos.organizations.getOrganization(activeMembership.organizationId);
     organizationsNames[organization.id] = organization.name;
   }
+
 
   return (
     <div className="container">
       <div>
         <h2 className="text-lg mt-6">Your companies</h2>
-        <p className="text-gray-500 text-sm mb-2">
-          Select a company to create a job add for
-        </p>
+        <p className="text-gray-500 text-sm mb-2">Select a company to create a job add for</p>
         <div>
           <div className="border inline-block rounded-md">
-            {Object.keys(organizationsNames).map((orgId) => (
+            {Object.keys(organizationsNames).map(orgId => (
               <Link
-                href={"/new-listing/" + orgId}
+                href={'/new-listing/' + orgId}
                 className={
-                  "py-2 px-4 flex gap-2 items-center " +
-                  (Object.keys(organizationsNames)[0] === orgId
-                    ? ""
-                    : "border-t")
-                }
-              >
+                  "py-2 px-4 flex gap-2 items-center "
+                  + (Object.keys(organizationsNames)[0] === orgId ? '' : 'border-t')
+                }>
                 {organizationsNames[orgId]}
                 <FontAwesomeIcon className="h-4" icon={faArrowRight} />
               </Link>
@@ -64,8 +63,7 @@ export default async function NewListingPage() {
 
         <Link
           className="inline-flex gap-2 items-center bg-gray-200 px-4 py-2 rounded-md mt-6"
-          href={"/new-company"}
-        >
+          href={'/new-company'}>
           Create a new company
           <FontAwesomeIcon className="h-4" icon={faArrowRight} />
         </Link>
