12df5165d9b6946c37f0a083e1f14bf4917f7dca
Tane4ka170 <tane4ka170@gmail.com>
2024-10-14 16:33:44 +0300

    Refactored JobRow component

--- src/app/components/JobRow.tsx
+++ src/app/components/JobRow.tsx
@@ -1,32 +1,60 @@
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+'use client';
+import TimeAgo from "@/app/components/TimeAgo";
+import {Job, JobModel} from "@/models/Job";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import Link from "next/link";
 
-export default function JobRow() {
+export default function JobRow({jobDoc}:{jobDoc:Job}) {
   return (
     <>
       <div className="bg-white p-4 rounded-lg shadow-sm relative">
-        <div className="absolute top-2 right-4 cursor-pointer">
-          <FontAwesomeIcon icon={faHeart} className="size-4 text-neutral-300" />
+        <div className="absolute cursor-pointer top-4 right-4">
+          <FontAwesomeIcon className="size-4 text-gray-300" icon={faHeart} />
         </div>
-        <div className="flex gap-4 grow">
-          <div className="content-center">
+        <div className="flex grow gap-4">
+          <div className="content-center w-12 basis-12 shrink-0">
             <img
-              src="https://freepnglogo.com/images/all_img/1717224836starbucks-logo-transparent.png"
-              alt="logo"
               className="size-12"
-            />
+              src={jobDoc?.jobIcon} alt=""/>
           </div>
           <div className="grow sm:flex">
             <div className="grow">
-              <div className="text-neutral-500 text-sm">Starbucks</div>
-              <div className="font-bold mb-1 text-lg">Barista</div>
-              <div className="text-neutral-400 text-sm">
-                On-site &middot; Evesham, UK &middot; Full-time, part-time
+              <div>
+                <Link href={`/jobs/${jobDoc.orgId}`} className="hover:underline text-gray-500 text-sm">{jobDoc.orgName || '?'}</Link>
+              </div>
+              <div className="font-bold text-lg mb-1">
+                <Link className="hover:underline" href={'/show/'+jobDoc._id}>{jobDoc.title}</Link>
+              </div>
+              <div className="text-gray-400 text-sm capitalize">
+                {jobDoc.remote}
+                {' '}&middot;{' '}
+                {jobDoc.city}, {jobDoc.country}
+                {' '}&middot;{' '}
+                {jobDoc.type}-time
+                {jobDoc.isAdmin && (
+                  <>
+                    {' '}&middot;{' '}
+                    <Link href={'/jobs/edit/'+jobDoc._id}>Edit</Link>
+                    {' '}&middot;{' '}
+                    <button
+                      type="button"
+                      onClick={async () => {
+                        await axios.delete('/api/jobs?id='+jobDoc._id);
+                        window.location.reload();
+                      }}>
+                      Delete
+                    </button>
+                  </>
+                )}
               </div>
             </div>
-            <div className="content-end text-neutral-500 text-xs">
-              2 weeks ago
-            </div>
+            {jobDoc.createdAt && (
+              <div className="content-end text-gray-500 text-sm">
+                <TimeAgo createdAt={jobDoc.createdAt} />
+              </div>
+            )}
           </div>
         </div>
       </div>
